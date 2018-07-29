90c5de7b86842c4a0cc1a844987b34eb8f0aa493
Ambrose Ledbrook <30817295+ambroseled@users.noreply.github.com>
2018-07-29 12:21:27 +1200

    Working on setting up server

---
+++ server.py
@@ -0,0 +1,51 @@
+import datetime
+import socket
+import sys
+
+MAGIC_NUMBER = 0x497E
+REQUEST_PACKET = 0x0001
+RESPONSE_PACKET = 0x0002
+DATE_REQUEST = 0x0001
+TIME_REQUEST = 0x0002
+REQUEST_PACKET_SIZE = 6
+ENGLISH_CODE = 0x0001
+MAORI_CODE = 0x0002
+GERMAN_CODE = 0x0003
+
+
+
+def get_time():
+    time = datetime.datetime.now()
+    year = str(time.year)
+    month = str(time.month)
+    day = str(time.day)
+    hour = str(time.hour)
+    minute = str(time.minute)
+
+
+def get_ports():
+    ports = input("Enter port numbers in following order: English, Te reo, German: ")
+    a, b, c = ports.split()
+    a, b, c = int(a), int(b), int(c)
+    invalid = False
+    for port in [a, b, c]:
+        if port < 1024 or port > 64000:
+            invalid = True
+    if a == b or a == c or b == c: invalid = True
+    if invalid:
+        print("Invalid port numbers entered")
+        sys.exit()
+    else:
+        return a, b, c
+
+
+english_port, maori_port, german_port = get_ports()
+
+english_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+maori_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+german_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+
+while True:
+
+
